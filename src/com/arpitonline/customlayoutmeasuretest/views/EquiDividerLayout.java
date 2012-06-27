37889669c4770a3152a4fdf9ac1b502b7daa8e4e
Arpit Mathur <mathur.arpit@gmail.com>
2012-06-27 00:38:45 -0400

    works correctly

--- src/com/arpitonline/customlayoutmeasuretest/views/EquiDividerLayout.java
+++ src/com/arpitonline/customlayoutmeasuretest/views/EquiDividerLayout.java
@@ -2,12 +2,9 @@ package com.arpitonline.customlayoutmeasuretest.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-
-import com.arpitonline.customlayoutmeasuretest.R;
 
 public class EquiDividerLayout extends ViewGroup {
 	
@@ -35,23 +32,16 @@ public class EquiDividerLayout extends ViewGroup {
 	
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
-		cll = (LinearLayout)findViewById(R.id.child_linear_layout);
-		Log.d(TAG, "onMeasure: "+widthMeasureSpec+", "+heightMeasureSpec);
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		cll.measure(widthMeasureSpec, heightMeasureSpec);
+		for(int i=0; i<getChildCount(); i++){
+			View v = getChildAt(i);
+			v.measure(widthMeasureSpec, heightMeasureSpec);
+		}
 	}
 
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
-		
-		if(changed){
-			invalidate();
-		}
-		
 		int itemWidth = (r-l)/getChildCount();
-		
-		Log.d(TAG, "Child LinearLayout Dimensions: "+cll.getWidth()+", "+cll.getHeight());
-		
 		for(int i=0; i< this.getChildCount(); i++){
 			View v = getChildAt(i);
 			v.layout(itemWidth*i, t, (i+1)*itemWidth, b);
