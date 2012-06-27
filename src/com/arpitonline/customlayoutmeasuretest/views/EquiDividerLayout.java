6c3315c2722bdbbb8bcce976fe7d57c384da8392
Arpit Mathur <mathur.arpit@gmail.com>
2012-06-27 01:11:05 -0400

    And now I learn even more. This seems to be the right way to override onMeasure

--- src/com/arpitonline/customlayoutmeasuretest/views/EquiDividerLayout.java
+++ src/com/arpitonline/customlayoutmeasuretest/views/EquiDividerLayout.java
@@ -2,6 +2,7 @@ package com.arpitonline.customlayoutmeasuretest.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -32,10 +33,27 @@ public class EquiDividerLayout extends ViewGroup {
 	
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+		
+//		At this time we need to call setMeasuredDimensions(). Lets just call the 
+//		parent View's method (see https://github.com/android/platform_frameworks_base/blob/master/core/java/android/view/View.java) 
+//		that does:
+//		setMeasuredDimension(getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec),
+//                getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec));
+//		
+		
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		
+
+		int wspec = MeasureSpec.makeMeasureSpec(getMeasuredWidth()/getChildCount(), MeasureSpec.EXACTLY);
+		int hspec = MeasureSpec.makeMeasureSpec(getMeasuredHeight(), MeasureSpec.EXACTLY);
+		
+		
 		for(int i=0; i<getChildCount(); i++){
 			View v = getChildAt(i);
-			v.measure(widthMeasureSpec, heightMeasureSpec);
+			Log.d(TAG, "Measured Width / Height: "+getMeasuredWidth()+", "+getMeasuredHeight());			
+			
+			
+			v.measure(wspec, hspec);
 		}
 	}
 
